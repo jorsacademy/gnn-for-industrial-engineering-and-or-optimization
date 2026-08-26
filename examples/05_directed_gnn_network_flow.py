efc7c62b40129dbc983944dc2e74b6b321e1777b
Jors Academy <abalci16@posta.pau.edu.tr>
2026-08-26 13:54:35 +0300

    Add directed flow example pointer

---
+++ examples/05_directed_gnn_network_flow.py
@@ -0,0 +1,5 @@
+"""See ../notebooks/05_directed_gnn_network_flow.ipynb.
+
+The notebook is the canonical implementation because it includes the full
+experiment, model comparison, pruning pipeline, and explanatory narrative.
+"""
